fef4ed16e37642f1f18dd375b081f641fba62975
Mobin Akhter <akhtemob@sheridancollege.ca>
2024-04-29 13:08:53 -0400

    Provided the structure to the application

--- name-sorter/Program.cs
+++ name-sorter/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Planning to have a main branch with the required solution, and another branch
+for the above and beyond solution for this coding assessment.
+
+TODO: Create a sample text file with unsorted name
+TODO: Read names from the text file
+TODO: Sort names from the txt file
+TODO: Print the sorted names to the screen
+TODO: Generate a txt file with the sorted names; overwrite it
+TODO: Add unit tests to make sure methods work as expected
+
+----------------------------------Submission Checklist----------------------------
+Comment code appropriately
+Create a readme documentation on GitHub 
+----------------------------------------------------------------------------------
+                     
+*/
